12d383cefa99de377ee948924e00b69a96fb8d68
Miller Love <mllove4@gmail.com>
2022-03-06 21:47:01 -0500

    Functionalized DBpedia Annotator and started testbed.

--- sandbox/DBpedia_Annotator_trial.py
+++ sandbox/DBpedia_Annotator_trial.py
@@ -1,49 +1,59 @@
-
 import spacy_dbpedia_spotlight
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 from db_core.database import Database
+from collections import defaultdict
 
 
-db = Database()
-nlp = spacy_dbpedia_spotlight.create('en')
+def annotator(keywords: 'list', confidence=0.5, verbose=False):
+    # create nlp pipe
+    nlp = spacy_dbpedia_spotlight.create('en')
+    nlp.get_pipe('dbpedia_spotlight').confidence = confidence
 
-##create list of keywords
-keywords = db.execute_sql(sql = '''
-        SELECT results FROM datalake.keyword_extraction_results
-        WHERE episode_uri_id = '68Eyz1s96eGvjbFATMOJyN' AND expirement_uuid = '11ecfe61-2777-4e0e-9d21-a480749d5003'
-''', return_list=True)
+    # convert keyword list into a 'document'
+    keywords_doc = " ".join(keywords)
 
-keywords = json.loads(keywords[0])['keywords']
+    # create nlp document
+    doc = nlp(keywords_doc)
 
-#convert keyword list into a 'document'
-keywords_doc = ", ".join(keywords)
+    # annotate keyword document
+    annotations = [(ent.text, ent.kb_id_, ent._.dbpedia_raw_result['@similarityScore']) for ent in doc.ents]
 
-print(keywords_doc)
+    if verbose:
+        print(keywords_doc)
+        print(annotations)
 
-#create nlp document
-doc = nlp(keywords_doc)
+    # get rdf:types of annotation resource
+    sparql_query = 'SELECT DISTINCT * WHERE { <REPLACE> a ?subject FILTER strstarts(str(?subject), str(yago:Wiki))}'
 
-#annotate keyword document
-annotations = [(ent.text, ent.kb_id_, ent._.dbpedia_raw_result['@similarityScore']) for ent in doc.ents]
+    results = defaultdict(list)
+    for i in annotations:
+        if i[0] in results.keys():
+            continue
+        sparql = SPARQLWrapper('http://dbpedia.org/sparql', returnFormat=JSON)
+        i_query = sparql_query.replace('REPLACE', str(i[1]))
+        sparql.setQuery(i_query)
 
-print(annotations)
+        sparql.setReturnFormat(JSON)
 
+        subjects = sparql.query().convert()
+        for j in subjects['results']['bindings']:
+            results[i[0]].append(j['subject']['value'][37:])
+            if verbose:
+                print(j)
 
-#get rdf:types of annotation resource
-sparql_query = 'SELECT DISTINCT * WHERE { <REPLACE> a ?subject FILTER strstarts(str(?subject), str(yago:Wiki))}'
-
-results = []
-for i in annotations:
-    print(i[1])
-    sparql = SPARQLWrapper('http://dbpedia.org/sparql', returnFormat=JSON)
-
-    sparql.setQuery(sparql_query.replace('REPLACE', str(i[1])))
-
-    sparql.setReturnFormat(JSON)
-
-    results.append(sparql.query().convert())
-
-print(results)
+    return results
 
 
+if __name__ == '__main__':
+    db = Database()
+
+    # create list of keywords
+    keyword_list = db.execute_sql(sql='''
+            SELECT results FROM datalake.keyword_extraction_results
+            WHERE episode_uri_id = '3aapVuXCEtmrR89oa8m2BS' AND expirement_uuid = 'aadb5075-3047-4a64-9538-a62d90947851'
+    ''', return_list=True)
+
+    keyword_list = json.loads(keyword_list[0])['keywords']
+
+    print(annotator(keyword_list, confidence=0.5, verbose=True))
