16e87a66519cb33907ff215034a757e0ba0a2000
Miller Love <mllove4@gmail.com>
2022-03-15 21:41:01 -0400

    cosine_similarity test

--- sandbox/cosine_similarity_test.py
+++ sandbox/cosine_similarity_test.py
@@ -7,19 +7,25 @@ list_1 = ['apple', 'pear', 'peach', 'blueberry', 'strawberry', 'potato', 'brocco
 list_2 = ['pineapple', 'pear', 'orange', 'blueberry', 'squash', 'apple', 'sprout']
 
 t0 = time.time()
-CounterA = Counter(list_1)
-CounterB = Counter(list_2)
+
 
 def counter_cosine_similarity(c1, c2):
+    c1 = Counter(c1)
+    c2 = Counter(c2)
     terms = set(c1).union(c2)
     dotprod = sum(c1.get(k, 0) * c2.get(k, 0) for k in terms)
     magA = math.sqrt(sum(c1.get(k, 0)**2 for k in terms))
     magB = math.sqrt(sum(c2.get(k, 0)**2 for k in terms))
     return dotprod / (magA * magB)
 
+
 i = 0
-while i <1000:
-    counter_cosine_similarity(CounterA, CounterB)
-    i+=1
+
+while i < 1000:
+    counter_cosine_similarity(list_1, list_2)
+    i += 1
 t1 = time.time()
+
 print(t1-t0)
+print(counter_cosine_similarity(list_1, list_2))
+
