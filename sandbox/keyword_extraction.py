e16f0ce3530943963c79156bae63be43c86bddf2
Tyler <twoolley3@gatech.edu>
2022-03-16 10:37:59 -0400

    add comments

--- sandbox/keyword_extraction.py
+++ sandbox/keyword_extraction.py
@@ -14,6 +14,7 @@ import math
 
 db = Database()
 
+# set yake parameters
 language = "en"
 max_ngram_size = 2
 deduplication_threshold = 0.7
@@ -31,7 +32,7 @@ podcast_custom_stop_words = ["actually", "only", "guys", "hold", "saying",
 
 stop_words = custom_stopwords.stopwords_starter_list + podcast_custom_stop_words
 
-# pulls in unique show ids
+# pulls in unique show ids having at least 10 episodes
 shows = db.execute_sql('''
     select distinct show_uri_id
     from datalake.raw_podcast_transcripts
@@ -39,6 +40,7 @@ shows = db.execute_sql('''
     having count(*) >= 10;
 ''', return_list=True)
 
+# run a sample of the shows
 shows = shows[:10]
 list_of_shows_and_keywords = []
 count = 1
@@ -54,6 +56,7 @@ for show in shows:
     seqm_keyword_lists = []
     show_keyword_dict = dict()
 
+    # set the keyword extractor with parameters
     custom_kw_extractor = yake.KeywordExtractor(lan=language,
                                                 n=max_ngram_size,
                                                 dedupLim=deduplication_threshold,
@@ -63,33 +66,40 @@ for show in shows:
                                                 stopwords=stop_words,
                                                 windowsSize=windows_size,)
 
+    # grab all the episodes for the current show
     episodes = db.execute_sql('''
             select episode_uri_id
             from datalake.raw_podcast_transcripts
             WHERE show_uri_id = '{REPLACEME_ID}';
         '''.format(REPLACEME_ID=show), return_list=True)
 
+    # loop through each episode for the current show
     for id in episodes:
         keyword_list = []
 
+        # get the transcript for the current episode
         text = db.execute_sql('''
         select lower(transcript)
         from datalake.raw_podcast_transcripts 
         where episode_uri_id = '{REPLACEME_ID}';
         '''.format(REPLACEME_ID=id), return_list=True)[0]
 
+        # get the keywords for the episode
         keywords = custom_kw_extractor.extract_keywords(text)
 
+        # put all the keywords into a list
         for keyword in keywords:
             keyword_list.append(keyword[0])
 
+        # add the episode's keywords to a list pertaining to the show
         seqm_keyword_lists.append(keyword_list)
 
+    # flatten and get most common keywords among all episodes in a show
     flattened = list(itertools.chain(*seqm_keyword_lists))
     counter_of_flat_list = Counter(flattened)
-
     show_most_common_words = counter_of_flat_list.most_common()
 
+    # only keep the top 20% most common keywords for each show
     len_most_common_words = len(show_most_common_words)
     twenty_percent_keys = math.floor(len_most_common_words * 0.2)
 
@@ -97,6 +107,7 @@ for show in shows:
     for k in range(twenty_percent_keys):
         final_lst.append(show_most_common_words[k][0])
 
+    # put each show and its keywords into a dictionary
     show_keyword_dict["show_id"] = show
     show_keyword_dict["keywords"] = final_lst
 
@@ -107,10 +118,11 @@ for show in shows:
 print("")
 print("Loading shows and keywords into database...")
 
-# load shows and keywords into table
+# load show ids, show names, and keywords into table
 for show_and_keywords in list_of_shows_and_keywords:
     show_id = show_and_keywords["show_id"]
 
+    # get show name
     show_name = db.execute_sql('''
             select distinct show_name
             from warehouse.podcast_metadata 
@@ -127,6 +139,7 @@ for show_and_keywords in list_of_shows_and_keywords:
     show_keyword_data['keywords'] = list(map(lambda x: json.dumps(x),
                                                show_keyword_data['keywords']))
 
+    # load data to datalake table
     show_keyword_data.to_sql('sample_shows_and_keywords', index=False,
                       schema='datalake', con=db.engine, if_exists="append")
 
