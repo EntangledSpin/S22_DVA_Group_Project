84c1f2aaf926e27c2b530d627c3afa38b2310c71
Miller Love <mllove4@gmail.com>
2022-03-16 19:44:16 -0400

    adjust for separate show_id

--- sandbox/Wiki_Category_writer.py
+++ sandbox/Wiki_Category_writer.py
@@ -2,22 +2,25 @@ import json
 from db_core.database import Database
 from sandbox.DBpedia_labeler import annotator
 import os
+import ast
 
 db = Database()
 sql_folder = os.path.join(os.path.abspath("."),"sql")
 writer_path = os.path.join(sql_folder, 'DBpedia_add.sql')
 writer_query = db.read_sql_path(writer_path)
 
-show_data = db.execute_sql(sql='SELECT show_and_keywords FROM datalake.sample_shows_and_keywords', return_list=True)
+shows = db.execute_sql(sql='SELECT show_id FROM datalake.sample_shows_and_keywords', return_list=True)
 
-for i, episode in enumerate(show_data):
-    keywords = json.loads(episode)['keywords']
-    WikiCat = annotator(keywords, confidence=0.6)
+for i, show in enumerate(shows):
+    keyword_list = db.execute_sql(sql = '''SELECT keywords FROM datalake.sample_shows_and_keywords 
+                                       WHERE show_id = '{replace}' '''.format(replace=show), return_list=True)
+    keywords = ast.literal_eval(keyword_list[0])
+    WikiCat = annotator(keywords, confidence=0.6, split_camel_case=True)
     Wiki_dict = {'Wiki_Categories': WikiCat}
     Wiki_json = json.dumps(Wiki_dict)
     Wiki_json = Wiki_json.replace("'", "''")
-    episode_clean = episode.replace("'", "''")
     print(i)
     writer = writer_query.replace("REPLACE1", Wiki_json)
-    writer = writer.replace("REPLACE2", episode_clean)
+    writer = writer.replace("REPLACE2", show)
     db.execute_sql(sql=writer, commit=True)
+
--- sandbox/sql/DBpedia_add.sql
+++ sandbox/sql/DBpedia_add.sql
@@ -1,3 +1,3 @@
 UPDATE datalake.sample_shows_and_keywords
-SET wiki_category = 'REPLACE1'
-WHERE show_and_keywords = 'REPLACE2'
+SET Wiki_Category_Split = 'REPLACE1'
+WHERE show_id = 'REPLACE2'
