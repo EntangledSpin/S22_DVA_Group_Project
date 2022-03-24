140ad91451c33c85f7906fedd326690c369f5eb4
Miller Love <mllove4@gmail.com>
2022-03-23 22:00:20 -0400

    Code for the Similarity Matrix -> Tableau pipeline.

--- sandbox/sql/edges_export.sql
+++ sandbox/sql/edges_export.sql
@@ -1,2 +1,3 @@
 SELECT show_id_1 AS Source, show_id_2 AS Target, 'Undirected' AS Type, 1 AS Weight, similarity, show_name_1, show_name_2
 FROM datalake.similarity_matrix_with_names
+WHERE similarity > 0
