1b0e656d6c66aafc30ccdb19dcf0445484f62d9d
Kerri <krobinson78@gatech.edu>
2022-03-30 09:40:15 -0400

    Table with show level detail to add to visualization

--- sandbox/sql/show_detail.sql
+++ sandbox/sql/show_detail.sql
@@ -1,9 +1,10 @@
 create table datalake.show_info as
 select
 show_id,
+show_name,
 show_description,
 publisher,
 sum(duration) as total_episode_length,
 count(episode_id) as number_of_episodes
 from warehouse.podcast_metadata
-group by 1,2,3
+group by 1,2,3,4
