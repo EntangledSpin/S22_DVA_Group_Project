8f852fb345a085f9f3a97af24505c6ab33c82072
Kerri <krobinson78@gatech.edu>
2022-04-04 14:11:22 -0400

    Brining in show details for all data

--- sandbox/sql/show_detail.sql
+++ sandbox/sql/show_detail.sql
@@ -1,12 +1,12 @@
-drop table if exists datalake.show_id_info;
-drop table if exists datalake.show_info;
+drop table if exists warehouse.show_id_info;
+drop table if exists warehouse.show_info;
 
-create table datalake.show_id_info as
+create table warehouse.show_id_info as
 select
 distinct(show_id)
 from warehouse.podcast_metadata;
 
-create table datalake.show_info as
+create table warehouse.show_info as
 select
 a.show_id,
 b.show_name,
@@ -15,6 +15,6 @@ b.publisher,
 CONCAT('https://open.spotify.com/show/', b.show_id) AS URL,
 avg(b.duration) as average_episode_length,
 count(b.episode_id) as number_of_episodes
-from datalake.show_id_info as a
+from warehouse.show_id_info as a
 left join  warehouse.podcast_metadata as b on a.show_id=b.show_id
 group by 1,2,3,4,5
