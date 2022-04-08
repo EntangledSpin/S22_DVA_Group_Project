c730ba2d73806c3aac7063fdcb50f9562c71be53
TLMozart3 <98718117+TLMozart3@users.noreply.github.com>
2022-04-08 11:11:24 -0400

    Delete Setup directory

--- Setup/create_sorted_shows_above_10.sql
+++
@@ -1,9 +0,0 @@
-/* run this prior to keyword extraction           */
-/* this table is needed in order to process shows */
-
-create table datalake.sorted_shows as
-select show_uri_id, count(*) as count
-from datalake.raw_podcast_transcripts
-group by show_uri_id
-having count(*) >= 10
-order by count, show_uri_id;
