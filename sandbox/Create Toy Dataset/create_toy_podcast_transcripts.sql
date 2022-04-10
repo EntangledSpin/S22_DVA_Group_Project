2a740e4ff14cdb24a2f8549dd80082770ab0c9da
Tyler <twoolley3@gatech.edu>
2022-04-10 11:53:30 -0400

    sql to sample 100 shows for toy dataset

---
+++ sandbox/Create Toy Dataset/create_toy_podcast_transcripts.sql
@@ -0,0 +1,16 @@
+/* create a toy data set of all transcripts for 100 random shows */
+/* that have at least 10 episodes each */
+
+/* pick 100 random show that have at least 10 episodes */
+create table datalake.toy_sorted_shows as
+select t1.*
+from datalake.sorted_shows t1
+order by random() limit 100;
+
+/* get all the transcripts for those 100 shows */
+create table datalake.toy_podcast_transcripts as
+select t1.*
+from datalake.raw_podcast_transcripts t1
+join datalake.toy_sorted_shows t2
+  on t1.show_uri_id = t2.show_uri_id
+order by t1.show_uri_id;
