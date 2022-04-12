9eeb0d5b3d42b50929e373743f0cdc2d34ff2e04
TLMozart3 <98718117+TLMozart3@users.noreply.github.com>
2022-04-11 23:37:44 -0400

    no longer needed

--- Production/Setup/nltk_wordnet_setup.py
+++
@@ -1,8 +0,0 @@
-# run this code prior to keyword extraction
-# a python installer should pop-up
-# in the installer, go to the Corpora tab
-# scroll down to Identifier = wordnet and double-click to install
-# nltk.corpus wordnet is now installed
-
-import nltk
-nltk.download()
